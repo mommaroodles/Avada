7c42fa9a3a00631c6c371b5adb93ee1b3ddc5c18
Melanie Shepherd <mommaroodles@users.noreply.github.com>
2024-12-12 19:34:22 +0200

    Fixed log-out link so that security nonces are added

--- Avada/templates/wc-top-user-container.php
+++ Avada/templates/wc-top-user-container.php
@@ -22,7 +22,7 @@ global $woocommerce, $current_user;
 					esc_attr__( 'Hello %1$s (not %2$s? %3$s)', 'Avada' ),
 					'<strong>' . esc_html( $current_user->display_name ) . '</strong></span><span class="not-user">',
 					esc_html( $current_user->display_name ),
-					'<a href="' . esc_url( wc_get_endpoint_url( 'customer-logout', '', wc_get_page_permalink( 'myaccount' ) ) ) . '">' . esc_attr__( 'Sign Out', 'Avada' ) . '</a>'
+					'<a href="' . esc_url( wc_get_account_endpoint_url( 'customer-logout', '', wc_get_page_permalink( 'myaccount' ) ) ) . '">' . esc_attr__( 'Sign Out', 'Avada' ) . '</a>'
 				);
 				?>
 			</span>
